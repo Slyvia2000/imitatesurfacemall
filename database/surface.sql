88506632c9f448cb41216adffa737a8427411285
Sylvia <862198345@qq.com>
2019-07-21 10:54:34 +0800

    新增  笔记本电脑列表

--- database/surface.sql
+++ database/surface.sql
@@ -26,7 +26,28 @@ CREATE TABLE surface_laptop_family(
 );
 -- 笔记本电脑列表
 CREATE TABLE surface_laptop(
-    
+    lid INT PRIMARY KEY AUTO_INCREMENT,
+    family_id INT,              #所属型号编号
+    title VARCHAR(128),         #主标题
+    subtitle VARCHAR(128),      #副标题
+    price DECIMAL(10,2),        #价格
+    promise VARCHAR(64),        #服务承诺
+    spec VARCHAR(64),           #规格/颜色
+
+    Iname VARCHAR(32),          #商品名称
+    os VARCHAR(32),             #操作系统
+    memory VARCHAR(32),         #内存容量
+    resolution VARCHAR(32),     #分辨率
+    video_card VARCHAR(32),     #显卡型号
+    cpu VARCHAR(32),            #处理器
+    video_memory VARCHAR(32),   #显存容量
+    category VARCHAR(32),       #所属分类
+    disk VARCHAR(32),           #硬盘容量及类型
+    details VARCHAR(1024),      #产品详细说明
+
+    shelf_time BIGINT,          #上架时间
+    sold_count INT,             #已售出的数量
+    is_onsale BOOLEAN           #是否促销中
 );
 
 -- 数据导入====>
